41d93c273335e32cffeb87aeb31afe17d5093eb6
hannah_zeng <hzeng9310@gmail.com>
2024-08-25 17:27:35 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
 /**Create and add a footer */
 const footer = document.createElement("footer");
-
+document.body.appendChild(footer);
 const today = new Date();
 const thisYear = today.getFullYear();
 const copyRight = document.createElement("p");
 const symbol = "\u00A9";
 copyRight.textContent = `${symbol} Hannah Zeng ${thisYear}.`;
 footer.appendChild(copyRight);
-
-document.body.append(footer);
 
 /**Add Skills List. */
 const skills = [
@@ -30,4 +28,46 @@ for (let i = 0; i < skills.length; i++) {
   skillsList.appendChild(skill);
 }
 
+/**Handle Message Form Submit */
+const messageForm = document.getElementById("leave_message");
+const handleSubmit = (event) => {
+  event.preventDefault();
 
+  let usersName = event.target.usersName.value;
+  let usersEmail = event.target.usersEmail.value;
+  let usersMessage = event.target.usersMessage.value;
+
+  console.log(usersName);
+  console.log(usersEmail);
+  console.log(usersMessage);
+
+  const messageSection = document.getElementById("messages");
+  const messageList = messageSection.querySelector("ul");
+  const newMessage = document.createElement("li");
+
+  newMessage.innerHTML = `
+  <a href="mailto:${usersEmail}">${usersName}</a>
+  <span>: ${usersMessage}</span>
+`;
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Remove";
+  removeButton.setAttribute("type", "button");
+
+  removeButton.addEventListener("click", (event) => {
+    const entry = removeButton.parentNode;
+    entry.remove();
+  });
+
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+
+  if (messageList.children.length === 0) {
+    messageSection.style.display = "none";
+  }
+
+  // Clear the form
+  event.target.reset();
+};
+
+messageForm.addEventListener("submit", handleSubmit);
